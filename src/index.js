d94917eeba43a0b1d73a1b5dfd02e71b60163a63
TheCunningBeast <the.cunning.beast@gmail.com>
2024-02-22 09:04:54 +0000

    Create a general logic for games in index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+const MAX_ROUNDS = 3;
+
+const showMessage = (data) => console.log(data);
+const getAnswer = (question = '') => readlineSync.question(question);
+
+export default (makeGameCard) => {
+  const {
+    getTask,
+    getQuestion,
+    getCorrectAnswer,
+    isCorrectAnswer,
+    refreshQuestion,
+  } = makeGameCard();
+
+  showMessage('Welcome to the Brain Games!');
+  const name = getAnswer('May I have your name?: ');
+  showMessage(`Hello, ${name}`);
+  showMessage(getTask());
+  for (let i = 0; i < MAX_ROUNDS; i += 1) {
+    showMessage(`Question: ${getQuestion()}`);
+    const answer = getAnswer('Your answer: ');
+    if (!isCorrectAnswer(answer)) {
+      showMessage(`'${answer}' is wrong answer ;(. Correct answer was '${getCorrectAnswer()}'.`);
+      showMessage(`Let's try again, ${name}`);
+      return;
+    }
+    showMessage('Correct!');
+    refreshQuestion();
+  }
+  showMessage(`Congratulations, ${name}!`);
+};
